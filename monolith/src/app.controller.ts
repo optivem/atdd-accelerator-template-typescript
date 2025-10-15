8432cc1cb5d838c6a64a0d0dc94861de35aa6277
Valentina Jemuović <valentina.jemuovic@optivem.com>
2025-10-15 14:14:28 +0200

    Add getEcho endpoint to AppController

--- monolith/src/app.controller.ts
+++ monolith/src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('api/echo')
+  getEcho(): string {
+    return this.appService.getEcho();
+  }
 }
--- monolith/src/app.service.ts
+++ monolith/src/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getEcho(): string {
+    return 'Echo response';
+  }
 }
